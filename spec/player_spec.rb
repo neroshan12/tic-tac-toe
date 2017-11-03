622d8c4f490f32fe3167a68740ae1884dce02a50
Nero Siva <neroshan12@gmail.com>
2017-11-03 16:00:40 +0000

    Add player name tests and functionality

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,7 +2,12 @@ require 'player'
 
 describe Player do
   it 'intitialize' do
-    player = Player.new
+    player = Player.new("Nero")
     expect(player).to be_instance_of(Player)
+  end
+
+  it 'initializes with a name' do
+    player = Player.new("Nero")
+    expect(player.name).to eq("Nero")
   end
 end
